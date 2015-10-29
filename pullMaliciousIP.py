e5648cd09d73acc6151fb5b880ba642d593bcc30
Travis Smith <tsmith@tripwire.com>
2015-10-28 21:37:45 -0700

    Updating complete config
    Adding updates to complete installation

---
+++ pullMaliciousIP.py
@@ -0,0 +1,15 @@
+()#!/usr/bin/env python
+
+import urllib2, re
+
+def writeYAML():
+	yamlFile = open('/etc/logstash/maliciousIP.yaml','w')
+	url='http://www.malwaredomainlist.com/hostslist/ip.txt'
+	html = urllib2.urlopen(url)
+	for line in html.readlines():
+		line = re.sub('\\r|\\n','',line)
+		yamlFile.write("\"" + line + "\": \"YES\"" + "\n")
+	yamlFile.close()
+
+if __name__=="__main__":
+	writeYAML()
---
+++ pullTorIP.py
@@ -0,0 +1,20 @@
+import urllib2, os
+
+url = 'https://check.torproject.org/exit-addresses'
+
+def GetExit(url):
+	response = urllib2.urlopen(url)
+	if response.getcode() == 200:
+		yamlFile = open('/opt/logstash/torIP.yaml','w')
+		for line in response.readlines():
+			if line.startswith('ExitAddress'):
+				ip = line.split()[1]
+				yamlFile.write("\"" + ip + "\": \"YES\"" + "\n")
+		yamlFile.close
+#Main 
+def main():
+	GetExit(url)
+
+# call main
+if __name__ == '__main__':
+	main()
