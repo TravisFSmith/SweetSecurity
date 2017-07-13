2c0446cace20b1a3961e2a1f85c27df9c2f8d633
Travis Smith <tsmith@pdxtsmithm.tripwire.com>
2017-07-13 09:50:00 -0700

    Comment Cleanup

--- sweetSecurity/server/alert.py
+++ sweetSecurity/server/alert.py
@@ -29,20 +29,3 @@ def send(alertType,alertMessage,logID,logIndex):
 	data['csrf_token']=auth.content
 	response=session.post(url,data=data, verify=False, headers={"referer": url})
 	return response
-
-fileName='Malware.exe'
-md5='md5md5md5md5md5'
-sha1='sha1sha1sha1sha1sha1'
-fileCheckScore='666'
-internalNickname='Ring Doorbell'
-externalIP='1.2.3.4'
-alert='FileCheck.io Found a Malicious File\nFile Name: %s\nFile MD5: %s\nFile SHA1: %s\nFileCheckIO Reputation Score: %s' % (fileName,md5,sha1,fileCheckScore)
-#response=send('filecheck',alert,'AV0uJIO23T9D9HeOSUtW','logstash-2017.07.10')
-#print response.status_code
-#print response.text
-
-
-alert='Disk usage above 85%'
-#response=send('diskcheck',alert,None,None)
-#print response.status_code
-#print response.text
